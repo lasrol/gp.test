85bbe8a77b2c29d2e5b1b4c2be4185b3d9719699
Lasse Rolstad <lasse@novos.no>
2020-12-01 16:59:18 +0100

    Added task

--- Brreg/Controllers/OrganizationController.cs
+++ Brreg/Controllers/OrganizationController.cs
@@ -4,6 +4,10 @@ namespace Brreg.Controllers
 {
     public class OrganizationController : Controller
     {
+        // 1. Fetch org from brreg using IBrregHttpClient
+        // 2. Implement IMemoryCache and clear cache after 30 seconds, so we dont refetch data we already have
+        // 3. Validate and return badrequest if orgnumber is less then 9 digits
+        
         //https://data.brreg.no/enhetsregisteret/api/enheter/919300388
         // GET
         public IActionResult Index(string org)
